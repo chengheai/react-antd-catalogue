2744b5a1c3b66c5299fb897afd88f58e660ba3bc
chengheai <842385139@qq.com>
2018-07-24 15:42:36 +0800

    init commit

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -5,20 +5,7 @@ import { Button, Icon } from 'antd';
 function IndexPage() {
   return (
     <div style={{ width: 400, margin: '100px auto' }}>
-      <Button type="primary">Primary</Button>
-      <Button>Default</Button>
-      <Button type="dashed">Dashed</Button>
-      <Button type="danger">Danger</Button>
-      <Button type="primary" shape="circle" icon="search" />
-      <Button type="primary" icon="search">Search</Button>
-      <Button shape="circle" icon="search" />
-      <Button icon="search">Search</Button>
-      <br />
-      <Button shape="circle" icon="search" />
-      <Button icon="search">Search</Button>
-      <Button type="dashed" shape="circle" icon="search" />
-      <Button type="dashed" icon="search">Search</Button>
-      <Icon type="desktop" style={{fontSize:28, color:'#c00'}} />
+      <h1>hello react!</h1>
   </div>
   );
 }
